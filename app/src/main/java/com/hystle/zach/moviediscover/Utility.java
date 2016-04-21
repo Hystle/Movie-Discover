887b445a0cd6d8990f45b0d03c66831333964777
Zach <zachary07@gmail.com>
2016-04-21 11:19:53 -0300

    cast detail ui improved

--- app/src/main/java/com/hystle/zach/moviediscover/Utility.java
+++ app/src/main/java/com/hystle/zach/moviediscover/Utility.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
 import android.support.annotation.IntDef;
 import android.util.DisplayMetrics;
@@ -94,80 +92,6 @@ public class Utility {
         return null;
     }
 
-    public static String getGenreNameById(String s) {
-        String genreName;
-
-        if(s == null){
-            return "";
-        }
-
-        switch(s){
-            case "28":
-                genreName = "Action";
-                break;
-            case "12":
-                genreName = "Adventure";
-                break;
-            case "16":
-                genreName = "Animation";
-                break;
-            case "35":
-                genreName = "Comedy";
-                break;
-            case "80":
-                genreName = "Crime";
-                break;
-            case "99":
-                genreName = "Documentary";
-                break;
-            case "18":
-                genreName = "Drama";
-                break;
-            case "10751":
-                genreName = "Family";
-                break;
-            case "14":
-                genreName = "Fantasy";
-                break;
-            case "10769":
-                genreName = "Foreign";
-                break;
-            case "36":
-                genreName = "History";
-                break;
-            case "27":
-                genreName = "Horror";
-                break;
-            case "10402":
-                genreName = "Music";
-                break;
-            case "9648":
-                genreName = "Mystery";
-                break;
-            case "10749":
-                genreName = "Romance";
-                break;
-            case "878":
-                genreName = "Science Fiction";
-                break;
-            case "10770":
-                genreName = "TV Movie";
-                break;
-            case "53":
-                genreName = "Thriller";
-                break;
-            case "10752":
-                genreName = "War";
-                break;
-            case "37":
-                genreName = "Western";
-                break;
-            default:
-                genreName = "";
-        }
-        return genreName;
-    }
-
     @Retention(RetentionPolicy.SOURCE)
     @IntDef({DATA_STATUS_OK, DATA_STATUS_SERVER_DOWN, DATA_STATUS_SERVER_INVALID, DATA_STATUS_NO_CONNECTION, DATA_STATUS_UNKNOWN})
     public @interface DataStatus {}
@@ -189,22 +113,6 @@ public class Utility {
     public static @DataStatus int getLocationStatus(Context c){
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
         return sp.getInt(c.getString(R.string.pref_data_status_key), DATA_STATUS_UNKNOWN);
-    }
-
-    public static boolean isNetworkAvailable(Context c) {
-        ConnectivityManager cm =
-                (ConnectivityManager)c.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        return activeNetwork != null &&
-                activeNetwork.isConnectedOrConnecting();
-    }
-
-    public static void resetLocationStatus(Context c){
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
-        SharedPreferences.Editor spe = sp.edit();
-        spe.putInt(c.getString(R.string.pref_data_status_key), DATA_STATUS_UNKNOWN);
-        spe.apply();
     }
 
     /**
